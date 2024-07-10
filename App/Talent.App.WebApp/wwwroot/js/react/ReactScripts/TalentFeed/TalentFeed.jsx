836d67e8094e11e22730427202bc1e83133f8c4e
Ong Leong Thye <ong_lt6916@hotmail.com>
2024-07-11 00:15:59 +0800

    Commit for Advanced Task Module 2

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentFeed.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentFeed.jsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom';
 import Cookies from 'js-cookie'
 import TalentCard from '../TalentFeed/TalentCard.jsx';
-import { Loader } from 'semantic-ui-react';
+import { Loader, Icon, Container } from 'semantic-ui-react';
 import CompanyProfile from '../TalentFeed/CompanyProfile.jsx';
 import FollowingSuggestion from '../TalentFeed/FollowingSuggestion.jsx';
 import { BodyWrapper, loaderData } from '../Layout/BodyWrapper.jsx';
@@ -11,44 +11,101 @@ export default class TalentFeed extends React.Component {
     constructor(props) {
         super(props);
 
-        //let loader = loaderData
-        //loader.allowedUsers.push("Employer")
-        //loader.allowedUsers.push("Recruiter")
+        let loader = loaderData
+        loader.allowedUsers.push("Employer")
+        loader.allowedUsers.push("Recruiter")
 
-        //this.state = {
-        //    loadNumber: 5,
-        //    loadPosition: 0,
-        //    feedData: [],
-        //    watchlist: [],
-        //    loaderData: loader,
-        //    loadingFeedData: false,
-        //    companyDetails: null
-        //}
+        this.state = {
+            loadNumber: 5,            
+            loadPosition: 1179,
+            feedData: [],
+            watchlist: [],
+            loaderData: loader,
+            loadingFeedData: true,
+            companyDetails: null
+        }
 
-        //this.init = this.init.bind(this);
-
+        this.init = this.init.bind(this);
+        this.loadFeedData = this.loadFeedData.bind(this);
+        this.handleScroll = this.handleScroll.bind(this);
     };
 
-    //init() {
-    //    let loader = TalentUtil.deepCopy(this.state.loaderData)
-    //    loader.isLoading = false
+    init() {
+        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        loaderData.isLoading = false;
+        this.setState({ loaderData });
+        this.loadFeedData();
+    }
 
+    handleScroll() {
+        const win = $(window);
+        if ((($(document).height() - win.height()) == Math.round(win.scrollTop())) || ($(document).height() - win.height()) - Math.round(win.scrollTop()) == 1) {
+            this.setState({ loadingFeedData: true })
+            this.loadFeedData()
+        }
+    };
 
-    //}
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+        this.init()
+    };
 
-    //componentDidMount() {
-    //    window.addEventListener('scroll', this.handleScroll);
-    //    this.init()
-    //};
+    loadFeedData() {
+        const { feedData, loadPosition, loadNumber } = this.state;
 
-   
+        var cookies = Cookies.get('talentAuthToken');
+        $.ajax({
+           
+            url: 'https://mvpstandard-p.azurewebsites.net/profile/profile/getTalent',
+            
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            data: { position: loadPosition, number: loadNumber },
+            type: "GET",
+            success: function (res) {
+                const joinedData = feedData.concat(res.data);
+                this.setState({
+                    feedData: joinedData,
+                    loadPosition: loadPosition + loadNumber,
+                    loadingFeedData: false,
+                })
+            }.bind(this)
+        })
+    }
+
     render() {
-       
+        const { feedData, loadingFeedData } = this.state;
 
-        //return (
-        //    <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
-               
-        //    </BodyWrapper>
-        //)
+        return (
+            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+                <div className="ui grid talent-feed container">
+                    <div className="four wide column">
+                        <CompanyProfile />
+                    </div>
+                    <div className="eight wide column ">
+                        {feedData.length > 0 ? feedData.map((feed, index) => (
+                            <TalentCard key={index} feed={feed} />
+                        )) : (
+                            <p>No talent profile to display.</p> 
+                        )}
+
+                        {loadingFeedData ?
+                            <Container fluid className="center aligned">
+                                <Icon loading name='spinner' size="huge" />
+                            </Container>
+                            :
+                            null
+                        }
+                    </div>
+                    <div className="four wide column">
+                        <div className="ui card">
+                            <FollowingSuggestion />
+                        </div>
+                    </div>
+                </div>
+            </BodyWrapper >
+        )
     }
 }
